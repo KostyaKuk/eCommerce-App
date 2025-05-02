b501b6a6f8c8a716819fc8f87643c5e6315da0c1
Konstantin Kukushkin <k.k.o.lukoml@mail.ru>
2025-05-02 09:55:26 +0300

    feat: Create prettier file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import * as path from 'path';
-import { defineConfig } from 'vite'
+import * as path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
     alias: {
-      'react': path.resolve('./node_modules/react')
-    }
-  }
-})
+      react: path.resolve("./node_modules/react"),
+    },
+  },
+});
