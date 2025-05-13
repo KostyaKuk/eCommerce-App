0fe4dbf87a40be538e5345e3661245502690c539
Konstantin Kukukshkin <96070043+KostyaKuk@users.noreply.github.com>
2025-05-13 21:19:42 +0300

    feat: add user authentication with commercetools (#12)
    Co-authored-by: zSqualler <john199502031@gmail.com>

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,6 @@
 import { createApiBuilderFromCtpClient } from "@commercetools/platform-sdk";
 import { ctpClient } from "./BuildClient";
+import { ClientBuilder, createAuthForPasswordFlow, TokenCache, TokenStore } from "@commercetools/sdk-client-v2";
 
 const projectKey = import.meta.env.VITE_COMMERCETOOLS_PROJECT_KEY;
 const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({ projectKey });
@@ -11,5 +12,58 @@ export const getCategories = async () => {
   } catch (error) {
     console.error("API error:", error);
     throw error;
+  }
+};
+
+export const loginUser = async (email: string, password: string) => {
+  try {
+    let store: TokenStore | undefined = undefined;
+    const tokenCache: TokenCache = {
+      set(cache: TokenStore) {
+        store = cache;
+      },
+      get() {
+        return store ?? { token: "", refreshToken: "", expirationTime: 0 };
+      },
+    };
+
+    const passwordClient = new ClientBuilder()
+      .withProjectKey(projectKey)
+      .withMiddleware(
+        createAuthForPasswordFlow({
+          host: import.meta.env.VITE_COMMERCETOOLS_AUTH_URL,
+          projectKey,
+          credentials: {
+            clientId: import.meta.env.VITE_COMMERCETOOLS_CLIENT_ID,
+            clientSecret: import.meta.env.VITE_COMMERCETOOLS_CLIENT_SECRET,
+            user: { username: email, password },
+          },
+          scopes: [`manage_my_profile:${projectKey}`],
+          tokenCache,
+          fetch,
+        })
+      )
+      .withHttpMiddleware({ host: import.meta.env.VITE_COMMERCETOOLS_API_URL, fetch })
+      .withUserAgentMiddleware()
+      .build();
+
+    const apiRootPassword = createApiBuilderFromCtpClient(passwordClient).withProjectKey({ projectKey });
+
+    const response = await apiRootPassword.me().get().execute();
+
+    const tokens = tokenCache.get();
+
+    return {
+      customer: response.body,
+      accessToken: tokens?.token,
+      refreshToken: tokens?.refreshToken,
+    };
+  } catch (error) {
+    console.error("Login error:", error);
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+    if (errorMessage.includes("Customer account with the given credentials not found")) {
+      throw new Error("InvalidCredentials: Неверный email или пароль.");
+    }
+    throw new Error("General: Произошла ошибка. Попробуйте позже.");
   }
 };
