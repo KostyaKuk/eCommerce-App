87408af4c70711d73b2207cf5c6c9d6f37e204cd
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-05-14 18:08:13 +0300

    feat: add Toaster

--- src/api/createCustomer.ts
+++ src/api/createCustomer.ts
@@ -3,23 +3,8 @@ import { getAccessToken } from "./getAccessToken";
 const projectKey = import.meta.env.VITE_CTP_PROJECT_KEY;
 const apiUrl = import.meta.env.VITE_CTP_API_URL;
 
-export async function createCustomer() {
+export async function createCustomer(customerDraft) {
   const token = await getAccessToken();
-  const customerDraft = {
-    email: "user@example.com",
-    password: "securePassword123!",
-    firstName: "Иван",
-    lastName: "Петров",
-    addresses: [
-      {
-        country: "RU",
-        city: "Москва",
-        streetName: "Тверская",
-        postalCode: "125009",
-      },
-    ],
-    defaultShippingAddress: 0,
-  };
 
   const response = await fetch(`${apiUrl}/${projectKey}/customers`, {
     method: "POST",
@@ -29,9 +14,6 @@ export async function createCustomer() {
     },
     body: JSON.stringify(customerDraft),
   });
-  if (!response.ok) {
-    const error = await response.json();
-    throw new Error("Failed to create customer: " + JSON.stringify(error));
-  }
-  return await response.json();
+
+  return response;
 }
--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,14 +1,10 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import RegistrationForm from "../../components/RegistrationForm";
 import styles from "./styles.module.css";
-import { createCustomer } from "../../api/createCustomer";
 
 export const RegisterPage = () => {
   return (
     <div className={styles.container}>
-      <Button variant="contained" onClick={() => createCustomer()}>
-        createCustomerTest
-      </Button>
       <Typography sx={{ mt: 3, mb: 3 }} variant="h4">
         Create Account
       </Typography>
